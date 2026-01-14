0f12d00782ba8f443be6b4a2b9f610318143f560
RAJAMURUGAN-VS <rajamuruganvs24@gmail.com>
2026-01-14 19:22:11 +0530

    feat: implement login page UI

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -2,8 +2,55 @@ import './Login.css'
 import {Component} from 'react'
 
 class Login extends Component {
+  state = {username: '', password: ''}
+
+  onChangeUsername = event => {
+    this.setState({username: event.target.value})
+  }
+
+  onChangePassword = event => {
+    this.setState({password: event.target.value})
+  }
+
   render() {
-    return <div className="login-container">Login</div>
+    const {username, password} = this.state
+
+    return (
+      <div className="login-container">
+        <form className="login-form-container">
+          <img
+            src="https://assets.ccbp.in/frontend/react-js/logo-img.png"
+            alt="website logo"
+            className="website-logo"
+          />
+          <label htmlFor="username" className="login-form-label">
+            USERNAME
+          </label>
+          <input
+            id="username"
+            type="text"
+            placeholder="Username"
+            value={username}
+            className="login-form-input"
+            onChange={this.onChangeUsername}
+          />
+          <label htmlFor="username" className="login-form-label">
+            PASSWORD
+          </label>
+          <input
+            id="password"
+            type="password"
+            placeholder="Password"
+            value={password}
+            className="login-form-input"
+            onChange={this.onChangePassword}
+          />
+          <button type="submit" className="login-form-button">
+            Login
+          </button>
+        </form>
+      </div>
+    )
   }
 }
 
