2ff4beed4d533438a75dbd7df3d6b8bcb4968e16
RAJAMURUGAN-VS <rajamuruganvs24@gmail.com>
2026-01-15 09:34:23 +0530

    feat: add home page UI

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,10 +1,22 @@
 import './Home.css'
+import {Link} from 'react-router-dom'
 import Navbar from '../Navbar/Navbar'
 
 const Home = () => (
   <>
     <Navbar />
-    <h1>Home</h1>
+    <div className="home-container">
+      <h1 className="home-title">Find The Job That Fits Your Life</h1>
+      <p className="home-description">
+        Millions of people are searching for jobs, salary information, company
+        reviews. Find the job that fits your abilities and potential.
+      </p>
+      <Link to="/jobs">
+        <button type="button" className="find-job-button">
+          Find Jobs
+        </button>
+      </Link>
+    </div>
   </>
 )
 
