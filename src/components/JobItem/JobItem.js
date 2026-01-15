1c4ded32b0f3d376433ec1db9ca9b054462aae3e
RAJAMURUGAN-VS <rajamuruganvs24@gmail.com>
2026-01-15 16:34:55 +0530

    feat: fetch and render jobs list with job item cards

--- src/components/JobItem/JobItem.js
+++ src/components/JobItem/JobItem.js
@@ -1,5 +1,50 @@
 import './JobItem.css'
+import {AiFillStar} from 'react-icons/ai'
+import {MdLocationOn, MdEmail} from 'react-icons/md'
 
-const JobItem = () => <div>Job Item</div>
+const JobItem = props => {
+  const {jobItem} = props
+  const {
+    companyLogoUrl,
+    employmentType,
+    jobDescription,
+    location,
+    packagePerAnnum,
+    rating,
+    title,
+  } = jobItem
+
+  return (
+    <li className="job-item-container">
+      <div className="job-title-container">
+        <img src={companyLogoUrl} alt="company logo" className="company-logo" />
+        <div className="job-item-details">
+          <h1 className="job-title">{title}</h1>
+          <div className="star-container">
+            <AiFillStar className="star-icon" />
+            <p className="rating">{rating}</p>
+          </div>
+        </div>
+      </div>
+      <div className="job-details-container">
+        <div className="job-details">
+          <div className="job-details-element">
+            <MdLocationOn className="details-icon" />
+            <p className="details">{location}</p>
+          </div>
+          <div className="job-details-element">
+            <MdEmail className="details-icon" />
+            <p className="details">{employmentType}</p>
+          </div>
+        </div>
+        <p className="description-title">{packagePerAnnum}</p>
+      </div>
+      <hr />
+      <p className="description-title">Description</p>
+      <p className="description">{jobDescription}</p>
+    </li>
+  )
+}
 
 export default JobItem
+
--- src/components/JobsList/JobsList.js
+++ src/components/JobsList/JobsList.js
@@ -1,10 +1,60 @@
 import './JobsList.css'
 import {Component} from 'react'
+import Cookies from 'js-cookie'
+import {IoMdSearch} from 'react-icons/io'
+import JobItem from '../JobItem/JobItem'
 
 class JobsList extends Component {
+  state = {jobsList: []}
+
+  componentDidMount() {
+    this.getJobsList()
+  }
+
+  getJobsList = async () => {
+    const jwtToken = Cookies.get('jwt_token')
+    const url = 'https://apis.ccbp.in/jobs'
+    const options = {
+      headers: {
+        Authorization: `Bearer ${jwtToken}`,
+      },
+      method: 'GET',
+    }
+    const response = await fetch(url, options)
+    const data = await response.json()
+    const {jobs} = data
+    const formattedJobsList = jobs.map(each => ({
+      companyLogoUrl: each.company_logo_url,
+      employmentType: each.employment_type,
+      id: each.id,
+      jobDescription: each.job_description,
+      location: each.location,
+      packagePerAnnum: each.package_per_annum,
+      rating: each.rating,
+      title: each.title,
+    }))
+    console.log(formattedJobsList)
+    this.setState({jobsList: formattedJobsList})
+  }
+
   render() {
-    return <div>Jobs List</div>
+    const {jobsList} = this.state
+
+    return (
+      <div className="jobs-list">
+        <div className="job-search-container">
+          <input type="search" placeholder="Search" className="job-search" />
+          <IoMdSearch className="search-icon" />
+        </div>
+        <ul className="jobs-list-container">
+          {jobsList.map(each => (
+            <JobItem key={each.id} jobItem={each} />
+          ))}
+        </ul>
+      </div>
+    )
   }
 }
 
 export default JobsList
+
