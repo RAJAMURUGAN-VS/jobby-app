91a92011fcc825a6f1ef30ecbbd0545ca86c9965
RAJAMURUGAN-VS <rajamuruganvs24@gmail.com>
2026-01-16 18:22:52 +0530

    feat: add loading and failure states to profile card

--- src/components/Jobs/ProfileCard.js
+++ src/components/Jobs/ProfileCard.js
@@ -1,15 +1,24 @@
 import './ProfileCard.css'
-import { Component } from 'react'
+import {Component} from 'react'
 import Cookies from 'js-cookie'
+import {ThreeDots} from 'react-loader-spinner'
+
+const apiStatusConstants = {
+  initial: 'INITIAL',
+  success: 'SUCCESS',
+  failure: 'FAILURE',
+  inProgress: 'IN_PROGRESS',
+}
 
 class ProfileCard extends Component {
-  state = {profileDetails: []}
+  state = {profileDetails: {}, apiStatus: apiStatusConstants.initial}
 
   componentDidMount() {
     this.getProfileDetails()
   }
 
   getProfileDetails = async () => {
+    this.setState({apiStatus: apiStatusConstants.inProgress})
     const jwtToken = Cookies.get('jwt_token')
     const url = 'https://apis.ccbp.in/profile'
     const options = {
@@ -19,22 +28,27 @@ class ProfileCard extends Component {
       method: 'GET',
     }
     const response = await fetch(url, options)
-    const data = await response.json()
-    const formattedData = {
-      profileDetails: data.profile_details,
-    }
-    const {profileDetails} = formattedData
-    const formattedProfileDetails = {
-      name: profileDetails.name,
-      profileImageUrl: profileDetails.profile_image_url,
-      shortBio: profileDetails.short_bio,
-    }
     if (response.ok) {
-      this.setState({profileDetails: formattedProfileDetails})
+      const data = await response.json()
+      const formattedData = {
+        profileDetails: data.profile_details,
+      }
+      const {profileDetails} = formattedData
+      const formattedProfileDetails = {
+        name: profileDetails.name,
+        profileImageUrl: profileDetails.profile_image_url,
+        shortBio: profileDetails.short_bio,
+      }
+      this.setState({
+        profileDetails: formattedProfileDetails,
+        apiStatus: apiStatusConstants.success,
+      })
+    } else {
+      this.setState({apiStatus: apiStatusConstants.failure})
     }
   }
 
-  render() {
+  renderSuccessView = () => {
     const {profileDetails} = this.state
     const {name, profileImageUrl, shortBio} = profileDetails
 
@@ -45,6 +59,39 @@ class ProfileCard extends Component {
         <p className="profile-description">{shortBio}</p>
       </div>
     )
+  }
+
+  renderLoadingView = () => (
+    <div className="products-loader-container">
+      <ThreeDots color="#f8fafc" height={50} width={50} />
+    </div>
+  )
+
+  renderFailureView = () => (
+    <div className="products-loader-container">
+      <button type="button" className="retry-button">
+        Retry
+      </button>
+    </div>
+  )
+
+  renderProfileView = () => {
+    const {apiStatus} = this.state
+
+    switch (apiStatus) {
+      case apiStatusConstants.success:
+        return this.renderSuccessView()
+      case apiStatusConstants.inProgress:
+        return this.renderLoadingView()
+      case apiStatusConstants.failure:
+        return this.renderFailureView()
+      default:
+        return null
+    }
+  }
+
+  render() {
+    return <div className="profile-card">{this.renderProfileView()}</div>
   }
 }
 
