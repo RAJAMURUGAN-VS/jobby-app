22bbc8261028f60d11c9e2ad1ecf8d116742c4d2
RAJAMURUGAN-VS <rajamuruganvs24@gmail.com>
2026-01-16 12:38:07 +0530

    feat: implement job item details page with API integration

--- src/components/JobItem/JobItem.js
+++ src/components/JobItem/JobItem.js
@@ -1,10 +1,12 @@
 import './JobItem.css'
+import {Link} from 'react-router-dom'
 import {AiFillStar} from 'react-icons/ai'
 import {MdLocationOn, MdEmail} from 'react-icons/md'
 
 const JobItem = props => {
   const {jobItem} = props
   const {
+    id,
     companyLogoUrl,
     employmentType,
     jobDescription,
@@ -15,36 +17,41 @@ const JobItem = props => {
   } = jobItem
 
   return (
-    <li className="job-item-container">
-      <div className="job-title-container">
-        <img src={companyLogoUrl} alt="company logo" className="company-logo" />
-        <div className="job-item-details">
-          <h1 className="job-title">{title}</h1>
-          <div className="star-container">
-            <AiFillStar className="star-icon" />
-            <p className="rating">{rating}</p>
+    <Link to={`/jobs/${id}`} className="link-element">
+      <li className="job-item-container">
+        <div className="job-title-container">
+          <img
+            src={companyLogoUrl}
+            alt="company logo"
+            className="company-logo"
+          />
+          <div className="job-item-details">
+            <h1 className="job-title">{title}</h1>
+            <div className="star-container">
+              <AiFillStar className="star-icon" />
+              <p className="rating">{rating}</p>
+            </div>
           </div>
         </div>
-      </div>
-      <div className="job-details-container">
-        <div className="job-details">
-          <div className="job-details-element">
-            <MdLocationOn className="details-icon" />
-            <p className="details">{location}</p>
+        <div className="job-details-container">
+          <div className="job-details">
+            <div className="job-details-element">
+              <MdLocationOn className="details-icon" />
+              <p className="details">{location}</p>
+            </div>
+            <div className="job-details-element">
+              <MdEmail className="details-icon" />
+              <p className="details">{employmentType}</p>
+            </div>
           </div>
-          <div className="job-details-element">
-            <MdEmail className="details-icon" />
-            <p className="details">{employmentType}</p>
-          </div>
+          <p className="description-title">{packagePerAnnum}</p>
         </div>
-        <p className="description-title">{packagePerAnnum}</p>
-      </div>
-      <hr />
-      <p className="description-title">Description</p>
-      <p className="description">{jobDescription}</p>
-    </li>
+        <hr />
+        <p className="description-title">Description</p>
+        <p className="description">{jobDescription}</p>
+      </li>
+    </Link>
   )
 }
 
 export default JobItem
-
--- src/components/JobItemDetails/JobItemDetails.js
+++ src/components/JobItemDetails/JobItemDetails.js
@@ -1,5 +1,168 @@
 import './JobItemDetails.css'
+import {Component} from 'react'
+import Cookies from 'js-cookie'
+import {AiFillStar} from 'react-icons/ai'
+import {MdLocationOn, MdEmail} from 'react-icons/md'
+import {FiExternalLink} from 'react-icons/fi'
+import Navbar from '../Navbar/Navbar'
 
-const JobItemDetails = () => <div>Job Item Details</div>
+class JobItemDetails extends Component {
+  state = {jobDetails: {skills: [], lifeAtCompany: {}}, similarJobs: []}
+
+  componentDidMount() {
+    this.getBlogItemdetails()
+  }
+
+  getBlogItemdetails = async () => {
+    const {match} = this.props
+    const {params} = match
+    const {id} = params
+    const url = `https://apis.ccbp.in/jobs/${id}`
+    const jwtToken = Cookies.get('jwt_token')
+    const options = {
+      headers: {
+        Authorization: `Bearer ${jwtToken}`,
+      },
+      method: 'GET',
+    }
+    const response = await fetch(url, options)
+    const data = await response.json()
+    const formattedData = {
+      jobDetails: data.job_details,
+      similarJobs: data.similar_jobs,
+    }
+    const {jobDetails, similarJobs} = formattedData
+
+    const formattedJobDetails = {
+      companyLogoUrl: jobDetails.company_logo_url,
+      companyWebsiteUrl: jobDetails.company_website_url,
+      employmentType: jobDetails.employment_type,
+      id: jobDetails.id,
+      jobDescription: jobDetails.job_description,
+      lifeAtCompany: {
+        description: jobDetails.life_at_company.description,
+        imageUrl: jobDetails.life_at_company.image_url,
+      },
+      location: jobDetails.location,
+      packagePerAnnum: jobDetails.package_per_annum,
+      rating: jobDetails.rating,
+      skills: jobDetails.skills.map(each => ({
+        imageUrl: each.image_url,
+        name: each.name,
+      })),
+      title: jobDetails.title,
+    }
+
+    const formattedSimilarJobs = similarJobs.map(each => ({
+      companyLogoUrl: each.company_logo_url,
+      employmentType: each.employment_type,
+      id: each.id,
+      jobDescription: each.job_description,
+      rating: each.rating,
+      title: each.title,
+    }))
+
+    this.setState({
+      jobDetails: formattedJobDetails,
+      similarJobs: formattedSimilarJobs,
+    })
+  }
+
+  renderSkills = () => {
+    const {jobDetails} = this.state
+    const {skills} = jobDetails
+
+    return (
+      <>
+        <p className="details-title">Skills</p>
+        <ul className="skills-list-container">
+          {skills.map(each => (
+            <li key={each.name} className="skills-container">
+              <img src={each.imageUrl} alt="skill" className="skill-logo" />
+              <p className="description">{each.name}</p>
+            </li>
+          ))}
+        </ul>
+      </>
+    )
+  }
+
+  renderLifeAtCompany = () => {
+    const {jobDetails} = this.state
+    const {lifeAtCompany} = jobDetails
+    const {description, imageUrl} = lifeAtCompany
+
+    return (
+      <>
+        <p className="details-title">Life at Company</p>
+        <div className="life-at-company-container">
+          <p className="details-description">{description}</p>
+          <img src={imageUrl} alt="comany" className="life-at-company-image" />
+        </div>
+      </>
+    )
+  }
+
+  render() {
+    const {jobDetails} = this.state
+    const {
+      companyLogoUrl,
+      companyWebsiteUrl,
+      employmentType,
+      jobDescription,
+      location,
+      packagePerAnnum,
+      rating,
+      title,
+    } = jobDetails
+
+    return (
+      <>
+        <Navbar />
+        <div className="job-item-details-container">
+          <div className="job-item-container">
+            <div className="job-title-container">
+              <img
+                src={companyLogoUrl}
+                alt="company logo"
+                className="company-logo"
+              />
+              <div className="job-item-details">
+                <h1 className="job-title">{title}</h1>
+                <div className="star-container">
+                  <AiFillStar className="star-icon" />
+                  <p className="rating">{rating}</p>
+                </div>
+              </div>
+            </div>
+            <div className="job-details-container">
+              <div className="job-details">
+                <div className="job-details-element">
+                  <MdLocationOn className="details-icon" />
+                  <p className="details">{location}</p>
+                </div>
+                <div className="job-details-element">
+                  <MdEmail className="details-icon" />
+                  <p className="details">{employmentType}</p>
+                </div>
+              </div>
+              <p className="description-title">{packagePerAnnum}</p>
+            </div>
+            <hr />
+            <div className="description-title-container">
+              <p className="details-title">Description</p>
+              <a href={companyWebsiteUrl} className="visit-container">
+                <p className="visit">Visit </p> <FiExternalLink />
+              </a>
+            </div>
+            <p className="details-description">{jobDescription}</p>
+            {this.renderSkills()}
+            {this.renderLifeAtCompany()}
+          </div>
+        </div>
+      </>
+    )
+  }
+}
 
 export default JobItemDetails
