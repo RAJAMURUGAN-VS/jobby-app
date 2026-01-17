e31a7a586abe1b34e38a35ae67982c5f751a8ce1
RAJAMURUGAN-VS <rajamuruganvs24@gmail.com>
2026-01-17 11:10:04 +0530

    feat: make navbar responsive and use logo as home link

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,6 +1,8 @@
 import './Navbar.css'
 import {Link, withRouter} from 'react-router-dom'
 import Cookies from 'js-cookie'
+import {MdHome, MdExitToApp} from 'react-icons/md'
+import {FaSuitcase} from 'react-icons/fa'
 
 const Navbar = props => {
   const onClickLogout = () => {
@@ -11,21 +13,32 @@ const Navbar = props => {
 
   return (
     <div className="nav-container">
-      <img
-        src="https://assets.ccbp.in/frontend/react-js/logo-img.png"
-        alt="website logo"
-        className="nav-website-logo"
-      />
+      <Link to="/" className="nav-logo-link">
+        <img
+          src="https://assets.ccbp.in/frontend/react-js/logo-img.png"
+          alt="website logo"
+          className="nav-website-logo"
+        />
+      </Link>
       <div className="nav-element-container">
-        <Link to="/" className="nav-element">
-          Home
+        <Link to="/" className="nav-link">
+          <p className="nav-element">Home</p>
+          <MdHome className="nav-home-icon" />
         </Link>
-        <Link to="/jobs" className="nav-element">
-          Jobs
+        <Link to="/jobs" className="nav-link">
+          <p className="nav-element">Jobs</p>
+          <FaSuitcase className="nav-login-icon" />
         </Link>
       </div>
       <button type="button" className="logout-button" onClick={onClickLogout}>
         Logout
+      </button>
+      <button
+        type="button"
+        className="logout-icon-button"
+        onClick={onClickLogout}
+      >
+        <MdExitToApp className="nav-logout-icon" />
       </button>
     </div>
   )
